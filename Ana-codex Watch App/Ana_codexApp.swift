2956db549817ac912eb3e0b173df5d4a23928152
Iñaki Abete <inakiabt@gmail.com>
2025-09-28 21:51:16 -0300

    Initial commit

---
+++ Ana-codex Watch App/Ana_codexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ana_codexApp.swift
+//  Ana-codex Watch App
+//
+//  Created by Iñaki Abete on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Ana_codex_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
