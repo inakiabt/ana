e3a769f7c29703c3a37d9780b6771e6f273ad301
Iñaki Abete <inakiabt@gmail.com>
2025-09-28 21:51:16 -0300

    Initial commit

---
+++ Ana-codex Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ana-codex Watch App
+//
+//  Created by Iñaki Abete on 28/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
